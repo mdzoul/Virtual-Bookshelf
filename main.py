a07af24281299dfa289f2d92cb0aee513747a321
Zoul Aimi <82052598+mdzoul@users.noreply.github.com>
2022-12-18 23:53:26 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,88 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, SelectField, URLField
+from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+import csv
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap(app)
+
+
+class BookDetails(FlaskForm):
+    title = StringField('Book Name', validators=[DataRequired()])
+    author = StringField('Book Author', validators=[DataRequired()])
+    rating = StringField('Rating', validators=[DataRequired()])
+    submit = SubmitField('Add Book')
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///new-books-collection.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.app_context().push()
+db = SQLAlchemy(app)
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+    def __repr__(self):
+        return f'Book: {self.title}'
+
+
+db.create_all()
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    # Read All Records
+    all_books = db.session.query(Book).all()
+    return render_template('index.html', books=all_books)
+
+
+@app.route("/add", methods=['GET', 'POST'])
+def add():
+    form = BookDetails()
+    if form.validate_on_submit():
+        # Create A New Record
+        new_book = Book(
+            title=f'{form.data["title"]}',
+            author=f'{form.data["author"]}',
+            rating=f'{form.data["rating"]}'
+        )
+        db.session.add(new_book)
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template('add.html', form=form)
+
+
+@app.route('/edit', methods=['GET', 'POST'])
+def edit():
+    if request.method == 'POST':
+        # Update A Record By PRIMARY KEY
+        book_id = request.form['id']
+        book_to_update = Book.query.get(book_id)
+        book_to_update.rating = request.form['rating']
+        db.session.commit()
+        return redirect(url_for('home'))
+    book_id = request.args.get('id')
+    book_selected = Book.query.get(book_id)
+    return render_template('edit.html', book=book_selected)
+
+
+@app.route('/delete')
+def delete():
+    # Delete A Particular Record By PRIMARY KEY
+    book_id = request.args.get('id')
+    book_to_delete = Book.query.get(book_id)
+    db.session.delete(book_to_delete)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
